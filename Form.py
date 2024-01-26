f75ca68f5d39c639c399d63672491ebf4eae69d6
Quade Witt <quade.witt@selu.edu>
2024-01-26 14:24:05 -0600

    simple email bot done

--- Form.py
+++ Form.py
@@ -2,6 +2,7 @@ import undetected_chromedriver as quiet_webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 from datetime import datetime
 now = datetime.now()
@@ -12,12 +13,11 @@ options.add_argument(f"user-data-dir={profile}")
 quiet_driver = quiet_webdriver.Chrome(options=options,use_subprocess=True)
 quiet_driver.get("https://mail.google.com/mail/u/0/#inbox?compose=new")
 quiet_driver.implicitly_wait(10)
-reply = quiet_driver.find_element(By.NAME, "Recipients")
 
-reply.send_keys("casey.gibson@selu.edu" + Keys.TAB + "From Snakey Boy" + Keys.TAB + 
-"Hello This was sent by snake man" + Keys.TAB)
-
-
+actions = ActionChains(quiet_driver)
+actions.send_keys("casey.gibson@selu.edu" + Keys.TAB + Keys.TAB + "From Snakey Boy" + Keys.TAB + 
+"Hello This was sent by snake man" + Keys.TAB + Keys.ENTER)
+actions.perform()
 
 
 
